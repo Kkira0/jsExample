1cec2148726c238262a8cd185d7e5d29d59d365d
pipisa <your@email.ex>
2024-11-12 15:55:44 +0200

    part 5

---
+++ part5/dom.js
@@ -0,0 +1,6 @@
+
+// Izveido HTML lapu ar vienu text ievades lauku, pogu un tukšu paragrāfu
+// Izveido script sadaļu, kurā izveido objektu ar 5 atribūtiem
+// Izmantojot bracket notation tehniku, izveido funkcionalitāti, 
+// kas ļauj lietotājam input laukā ievadīt vienu no 5 atribūtiem un, nospiežot pogu, atrast attiecīgā atribūta vērtību objektā.
+//  Vērtība jāizvada tukšajā paragrāfā. Piem., lietotājs ievada "model", noklikšķina un paragrāfa teksta paradās "Golf".
